b121f391e4cc135572f903089ca4c32d8f4ca442
Felix Scheinost <felix.scheinost@posteo.de>
2017-04-03 02:07:11 +0200

    Working?

--- board_differ.js
+++ board_differ.js
@@ -1,30 +1,34 @@
 const bmp = require('bmp-js')
 const colors = require('./colors')
+const Jimp = require('jimp')
 
-// Takes the 2 buffers and returns a list of
-// valid colors changes to make
+// Takes the 2 buffers and returns a valid paint to make
+// the targetBuffer meet the requiremetns
 
 let HEIGHT = 1000
 let WIDTH = 1000
-let TRANSPARENT = 0xff00ffff
 
 module.exports = function (rawBoardBuffer, rawTargetBuffer) {
   let targetBuffer = bmp.decode(rawTargetBuffer).data
   let boardBuffer = bmp.decode(rawBoardBuffer).data
 
-  let actions = []
   let len = targetBuffer.byteLength
+  let actions = []
   for (let i = 0; i < len-4; i += 4) {
     let val = targetBuffer.readUIntBE(i, 4)
-    if (val !== TRANSPARENT) {
+    let color = colors.byInt.indexOf(val)
+    let rgb = Jimp.intToRGBA(val)
+
+    // transparent
+    if (!(rgb.r == 255 && rgb.g == 0 && rgb.b == 255)) {
       let boardVal = boardBuffer.readUIntBE(i, 4)
       if (boardVal !== val) {
         let n = (i/4)
         let x = n % 1000
         let y = Math.floor(n / 1000)
-        let color = colors.byInt.indexOf(val)
-        if (color === -1) {
-          console.warn(`WARNING: Unknown color #${val.toString(16)} at X: ${x} Y: ${y}`)
+
+        if (color == -1) {
+          console.error(`Skipping invalid color (${rgb.r}, ${rgb.g}, ${rgb.b}) at X: ${x} Y: ${y}`)
         } else {
           actions.push({ x: x, y: y, color: color })
         }
